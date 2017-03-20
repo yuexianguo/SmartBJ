344811747553ed90730653a2a339b6317d69d8e2
yuexianguo <gyx685255>
2017-03-20 20:54:54 +0800

    新闻标题和时间的刷新

--- app/src/main/java/com/example/administrator/zhihuibj/widget/NewsPullToRefreshListView.java
+++ app/src/main/java/com/example/administrator/zhihuibj/widget/NewsPullToRefreshListView.java
@@ -6,10 +6,20 @@ import android.util.AttributeSet;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.Volley;
 import com.example.administrator.zhihuibj.R;
+import com.example.administrator.zhihuibj.bean.NewsListsBean;
+import com.example.administrator.zhihuibj.netWork.SmartBJRequest;
 import com.itheima.pulltorefreshlib.PullToRefreshListView;
 import com.leon.loopviewpagerlib.FunBanner;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by Administrator on 2017/3/19.
@@ -18,7 +28,9 @@ import com.leon.loopviewpagerlib.FunBanner;
 public class NewsPullToRefreshListView extends PullToRefreshListView {
 
     private int[] imageResIds = {R.mipmap.icon_1, R.mipmap.icon_2, R.mipmap.icon_3, R.mipmap.icon_4, R.mipmap.icon_5};
-
+    private String mUrl;
+    private NewsListsBean mNewsListsBean;
+    private static final String TAG = "NewsPullToRefreshListVi";
     public NewsPullToRefreshListView(Context context) {
         this(context, null);
     }
@@ -46,10 +58,16 @@ public class NewsPullToRefreshListView extends PullToRefreshListView {
             getRefreshableView().addHeaderView(funBanner);
     }
 
+
     private BaseAdapter mBaseAdapter = new BaseAdapter() {
         @Override
         public int getCount() {
-            return 30;
+
+            if(mNewsListsBean!=null){
+
+                return mNewsListsBean.getData().getNews().size();
+            }
+            return 0;
         }
 
         @Override
@@ -64,13 +82,70 @@ public class NewsPullToRefreshListView extends PullToRefreshListView {
 
         @Override     //view_news_list_item
         public View getView(int position, View convertView, ViewGroup parent) {
-
+            ViewHolder viewHolder = null;
             if(convertView==null){
 
                 convertView=View.inflate(getContext(), R.layout.view_news_list_item,null);
+                viewHolder = new ViewHolder(convertView);
+                convertView.setTag(viewHolder);
+            }else {
+                viewHolder = (ViewHolder) convertView.getTag();
             }
 
+            NewsListsBean.DataBean.NewsBean newsBean  =  mNewsListsBean.getData().getNews().get(position);
+           //标题
+            viewHolder.mTitle.setText(newsBean.getTitle());
+            viewHolder.mTitle.setTextColor(Color.BLACK);
+            //设置时间
+            viewHolder.mPublishTime.setText(newsBean.getPubdate());
+
             return convertView;
+        }
+    };
+
+    public class ViewHolder {
+        @BindView(R.id.title)
+        TextView mTitle;
+        @BindView(R.id.publishTime)
+        TextView mPublishTime;
+
+        public ViewHolder(View root){
+            ButterKnife.bind(this,root);
+        }
+
+    }
+
+    public void setUrl(String url) {
+        mUrl = url;
+        //创建请求
+        SmartBJRequest<NewsListsBean> request = new SmartBJRequest<>(Request.Method.GET,
+                NewsListsBean.class,
+                url,
+                null,
+                mNewsListsBeanListener,
+               mErrorListener
+                );
+        //发送网络请求：
+        Volley.newRequestQueue(getContext()).add(request);
+
+
+    }
+
+    private Response.Listener<NewsListsBean> mNewsListsBeanListener = new Response.Listener<NewsListsBean>() {
+        @Override
+        public void onResponse(NewsListsBean response) {
+            //Log.d(TAG, "NewsPullToRefreshListVi: "+response.getData().getNews().get(0).getTitle());
+           mNewsListsBean =  response;
+            //刷新
+            mBaseAdapter.notifyDataSetChanged();
+
+        }
+    };
+
+    private Response.ErrorListener mErrorListener = new Response.ErrorListener() {
+        @Override
+        public void onErrorResponse(VolleyError error) {
+
         }
     };
 }
