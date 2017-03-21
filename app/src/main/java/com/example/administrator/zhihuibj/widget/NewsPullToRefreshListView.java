e39985eb2992dc10ab1e86700daa4e7a3c01cb54
yuexianguo <gyx685255>
2017-03-21 17:30:21 +0800

    刷新轮播图，下拉刷新，上拉加载更多

--- app/src/main/java/com/example/administrator/zhihuibj/widget/NewsPullToRefreshListView.java
+++ app/src/main/java/com/example/administrator/zhihuibj/widget/NewsPullToRefreshListView.java
@@ -7,15 +7,22 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.Volley;
+import com.android.volley.toolbox.NetworkImageView;
 import com.example.administrator.zhihuibj.R;
+import com.example.administrator.zhihuibj.app.Constant;
 import com.example.administrator.zhihuibj.bean.NewsListsBean;
 import com.example.administrator.zhihuibj.netWork.NetworkListener;
+import com.example.administrator.zhihuibj.netWork.NetworkManager;
 import com.example.administrator.zhihuibj.netWork.SmartBJRequest;
+import com.itheima.pulltorefreshlib.PullToRefreshBase;
 import com.itheima.pulltorefreshlib.PullToRefreshListView;
 import com.leon.loopviewpagerlib.FunBanner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -26,10 +33,12 @@ import butterknife.ButterKnife;
 
 public class NewsPullToRefreshListView extends PullToRefreshListView {
 
-    private int[] imageResIds = {R.mipmap.icon_1, R.mipmap.icon_2, R.mipmap.icon_3, R.mipmap.icon_4, R.mipmap.icon_5};
+   // private int[] imageResIds = {R.mipmap.icon_1, R.mipmap.icon_2, R.mipmap.icon_3, R.mipmap.icon_4, R.mipmap.icon_5};
     private String mUrl;
     private NewsListsBean mNewsListsBean;
     private static final String TAG = "NewsPullToRefreshListVi";
+    private FunBanner mFunBanner;
+    private String mMore;
     public NewsPullToRefreshListView(Context context) {
         this(context, null);
     }
@@ -44,19 +53,106 @@ public class NewsPullToRefreshListView extends PullToRefreshListView {
 
         setAdapter(mBaseAdapter);
         setMode(Mode.BOTH);
-
+        int height = getResources().getDimensionPixelOffset(R.dimen.indicator_height);
         FunBanner.Builder builder = new FunBanner.Builder(getContext());
-        FunBanner funBanner = builder.setEnableAutoLoop(true)
-                .setImageResIds(imageResIds)
+        mFunBanner = builder.setEnableAutoLoop(true)
+
                 .setDotSelectedColor(Color.RED)
-                .setHeightWidthRatio(0.5556f)
+                .setHeightWidthRatio(0.5f)
                 .setLoopInterval(3000)
                 .setEnableAutoLoop(true)
+                .setIndicatorBarHeight(height)
                 .setIndicatorBackgroundColor(R.color.indicator_bg)
                 .build();
-            getRefreshableView().addHeaderView(funBanner);
+            getRefreshableView().addHeaderView(mFunBanner);
+             setOnRefreshListener(mOnRefreshListener2);
     }
 
+    private OnRefreshListener2 mOnRefreshListener2 = new OnRefreshListener2() {
+        @Override
+        public void onPullDownToRefresh(PullToRefreshBase pullToRefreshBase) {
+            //重新发送网络请求获取最新的数据
+            SmartBJRequest<NewsListsBean> request = new SmartBJRequest<NewsListsBean>(                NewsListsBean.class,
+                    mUrl,
+                    mRefreshListener);
+
+            NetworkManager.sendRequest(request);
+
+        }
+        //上拉加载更多
+        @Override
+        public void onPullUpToRefresh(PullToRefreshBase pullToRefreshBase) {
+            if(mMore.length()>0){
+                //可以下拉更多：
+                String url = Constant.HOST + mMore;
+                SmartBJRequest<NewsListsBean> request = new SmartBJRequest<NewsListsBean>(
+                        NewsListsBean.class,
+                        url,
+                        mLoadMoreListener
+                );
+                NetworkManager.sendRequest(request);
+            }else {
+                Toast.makeText(getContext(),"不能加载更多图片",Toast.LENGTH_SHORT).show();
+                //延迟刷新
+                post(new Runnable() {
+                    @Override
+                    public void run() {
+                        onRefreshComplete();//重置上拉刷新头
+                    }
+                });
+
+            }
+        }
+    };
+
+    private  NetworkListener<NewsListsBean> mLoadMoreListener = new NetworkListener<NewsListsBean>(){
+        @Override
+        public void onResponse(NewsListsBean response) {
+            Toast.makeText(getContext(), "加载更多数据成功", Toast.LENGTH_SHORT).show();
+            //保存mMore;
+            mMore = response.getData().getMore();
+            //加载的更多的数据加到原来的新闻的集合中
+            mNewsListsBean.getData().getNews().addAll(response.getData().getNews());
+            mBaseAdapter.notifyDataSetChanged();
+
+            onRefreshComplete();
+
+
+        }
+    };
+
+
+
+    private NetworkListener<NewsListsBean> mRefreshListener = new NetworkListener<NewsListsBean>(){
+        @Override
+        public void onResponse(NewsListsBean response) {
+            //加载网络数据
+            updateNewsList(response);
+            //刷新图片复位
+            onRefreshComplete();//下拉刷新结束
+        }
+    };
+
+
+    private  void  updateNewsList(NewsListsBean response){
+        mNewsListsBean = response;
+        //刷新
+        mBaseAdapter.notifyDataSetChanged();
+
+        //刷新轮播图
+
+        List<String> imageList = new ArrayList<String>();
+        List<String> titleList = new ArrayList<String>();
+        List<NewsListsBean.DataBean.TopnewsBean> topnewsBean  = mNewsListsBean.getData().getTopnews();
+        for(int i=0;i<topnewsBean.size();i++){
+            titleList.add(topnewsBean.get(i).getTitle());
+            imageList.add(topnewsBean.get(i).getTopimage().replace("10.0.2.2","10.0.3.2"));
+
+        }
+        mFunBanner.setImageUrlsAndTitles(imageList,titleList);
+        mMore=mNewsListsBean.getData().getMore();
+
+    }
 
     private BaseAdapter mBaseAdapter = new BaseAdapter() {
         @Override
@@ -96,8 +192,10 @@ public class NewsPullToRefreshListView extends PullToRefreshListView {
             viewHolder.mTitle.setText(newsBean.getTitle());
             viewHolder.mTitle.setTextColor(Color.BLACK);
             //设置时间
+            //新闻列表里的图片和标题的显示
             viewHolder.mPublishTime.setText(newsBean.getPubdate());
-
+            String url = newsBean.getListimage().replace("10.0.2.2","10.0.3.2");
+            viewHolder.mNetworkImageView.setImageUrl(url,NetworkManager.getImageLoder());
             return convertView;
         }
     };
@@ -107,6 +205,8 @@ public class NewsPullToRefreshListView extends PullToRefreshListView {
         TextView mTitle;
         @BindView(R.id.publishTime)
         TextView mPublishTime;
+        @BindView(R.id.news_images)
+        NetworkImageView mNetworkImageView;
 
         public ViewHolder(View root){
             ButterKnife.bind(this,root);
@@ -123,7 +223,8 @@ public class NewsPullToRefreshListView extends PullToRefreshListView {
                 mNewsListsBeanListener
                 );
         //发送网络请求：
-        Volley.newRequestQueue(getContext()).add(request);
+        NetworkManager.sendRequest(request);
+        //Volley.newRequestQueue(getContext()).add(request);
 
 
     }
@@ -131,9 +232,8 @@ public class NewsPullToRefreshListView extends PullToRefreshListView {
     private  NetworkListener<NewsListsBean> mNewsListsBeanListener = new NetworkListener<NewsListsBean>(){
         @Override
         public void onResponse(NewsListsBean response) {
-            mNewsListsBean =  response;
-            //刷新
-            mBaseAdapter.notifyDataSetChanged();
+
+            updateNewsList(response);
         }
 
         @Override
